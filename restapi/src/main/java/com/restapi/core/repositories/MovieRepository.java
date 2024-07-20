7d69d789a24bed27212cc93955ba376ea5fd3d72
miguel <ninosilvagpt@gmail.com>
2024-07-20 15:11:15 -0500

    :sparkles: Se agregó Modelos y repositorios para probar

---
+++ restapi/src/main/java/com/restapi/core/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.restapi.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.restapi.core.models.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
