865df97c59235788c24537e8542bfcb835229adf
miguel <ninosilvagpt@gmail.com>
2024-07-21 14:30:49 -0500

    :sparkles: Cambio de contexto por anotaciones con Spring Annotations

---
+++ cursospring/src/main/java/com/cursospring/cursospring/anotaciones/EmpleadosConfig.java
@@ -0,0 +1,10 @@
+package com.cursospring.cursospring.anotaciones;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cursospring.cursospring.anotaciones")
+public class EmpleadosConfig {
+
+}
--- cursospring/src/main/java/com/cursospring/cursospring/anotaciones/UsoAnotaciones.java
+++ cursospring/src/main/java/com/cursospring/cursospring/anotaciones/UsoAnotaciones.java
@@ -1,12 +1,17 @@
 package com.cursospring.cursospring.anotaciones;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UsoAnotaciones {
 	public static void main(String[] args) {
+		/*
 		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext(
-				"applicationContextAnotations.xml");
+				"applicationContextAnotations.xml");*/
 
+		//leer archivo de configuración
+		AnnotationConfigApplicationContext contexto = new AnnotationConfigApplicationContext(EmpleadosConfig.class);
+		
 		// Pedir un bean
 		Empleados Miguel = contexto.getBean("ComercialExperimentado", Empleados.class);
 		Empleados Lucia = contexto.getBean("ComercialExperimentado", Empleados.class);
